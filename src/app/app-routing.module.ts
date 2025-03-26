90f68b0464d062f829831a1de23488bf1262007e
kpk2714 <krishnapadakar05@gmail.com>
2025-03-26 16:27:41 +0530

    AuthGuard-New-Process

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthGuardService } from './guard/auth-guard.service';
+import { canActivate } from './guard/auth-guard';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path:'login', component: LoginComponent},
-  {path: 'dashboard', component : DashboardComponent, canActivate: [AuthGuardService]},
+  // {path: 'dashboard', component : DashboardComponent, canActivate: [AuthGuardService]},
+  {path: 'dashboard', component : DashboardComponent, canActivate: [canActivate]},
   {path: '**', component: LoginComponent}
 ];
 
